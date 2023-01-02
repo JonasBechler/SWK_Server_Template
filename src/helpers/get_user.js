3d8f28736b7fb605e7bb2dbc66bd825dacba5407
Jonas Bechler <jonasbechler@gmail.com>
2023-01-02 09:53:16 +0100

    EdgeCases_user_by_faid

--- src/helpers/get_user.js
+++ src/helpers/get_user.js
@@ -16,11 +16,30 @@ module.exports = function(userDataPath){
         return {details: userData.details, user: selected_user }
     }
 
+    function getUserByFusionAuthID(fa_id) {
+
+        const userData = JSON.parse(fs.readFileSync(userDataPath, {encoding:'utf8', flag:'r'}));
+        var selected_user = null;
+        
+        userData.users.forEach(user => {
+            //TODO: fa_id
+            if (fa_id === user.email) {
+                selected_user = user;
+            }
+        });
+
+        return {details: userData.details, user: selected_user }
+    }
+
     function getDetails() {
         const userData = JSON.parse(fs.readFileSync(userDataPath, {encoding:'utf8', flag:'r'}));
 
         return {details: userData.details }
     }
 
-    return {by_mail: getUserByEmail, details: getDetails}
+    return {
+        by_mail: getUserByEmail, 
+        by_fusionauthID: getUserByFusionAuthID, 
+        details: getDetails
+    }
 }
