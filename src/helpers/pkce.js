b5b347d332f0af7df6ff2cfc14916a3605233226
JonasBechler <jonasbechler@gmail.com>
2023-01-29 23:21:03 +0100

    comments

--- src/helpers/pkce.js
+++ src/helpers/pkce.js
@@ -3,22 +3,29 @@
 
 const crypto = require('crypto');
 
+// Convert binary string to readable ASCII string for usage in urls (radix-64 representation)
+// "+" -> "-"
+// "/" -> "_"
+// "=" -> ""  because length of string is known
 function base64URLEncode(str) {
   return str
     .toString("base64")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=/g, "")
+    .replace(/\+/g, "-")  
+    .replace(/\//g, "_") 
+    .replace(/=/g, "")    
 }
 
+// Create sha256 hash from string
 function sha256(buffer) {
   return crypto.createHash("sha256").update(buffer).digest()
 }
 
+// Create 32 random bytes in base64 format as verifier
 const generateVerifier = () => {
   return base64URLEncode(crypto.randomBytes(32))
 }
 
+// Create sha256 hash from verifier in base64 format as challenge 
 const generateChallenge = (verifier) => {
   return base64URLEncode(sha256(verifier))
 }
