da9c3377d89f60685433a79be52ae054abc859a4
JonasBechler <jonasbechler@gmail.com>
2023-02-03 18:32:24 +0100

    updates

---
+++ src/intern/routes/connect_accounts.js
@@ -0,0 +1,37 @@
+
+module.exports = function( config, userDataPath ) {
+
+    const express = require('express');
+    const router = express.Router();
+
+    const get_user = require('../../helpers/get_user')(userDataPath)
+    const update_user = require('../../helpers/update_user')(userDataPath)
+    const fusionauth = require("../../SWK_Fusionauth_Handler/index")(config)
+
+
+
+    router.get('/', (req, res) => {
+        
+        if (req.session.account_id && req.session.token){
+            fusionauth.introspect(req.session.token)
+            .then(introspectResponse => {
+                if (introspectResponse.active){
+                    const selected_user = get_user.by_account_id(req.session.account_id);
+                    const selected_fusionauth_user = get_user.by_fusionauthIntrospect(introspectResponse);
+                    
+                    if (selected_user.user && selected_fusionauth_user.fusionauth_user){
+                    
+                        selected_user.user.knlogin_id = selected_fusionauth_user.fusionauth_user.knlogin_id
+                        console.log(selected_user.user);
+                        update_user(selected_user.user);
+                        res.redirect(`${config.device_ip}:${config.port}`);
+                        
+                    }
+                }
+            })
+            
+        }
+    });
+
+    return router
+}
---
+++ src/intern/routes/login.js
@@ -0,0 +1,27 @@
+
+module.exports = function( config, userDataPath ) {
+
+    const express = require('express');
+    const router = express.Router();
+
+    const get_user = require('../../helpers/get_user')(userDataPath)
+
+
+    router.post('/', (req, res) => {
+        let email = req.query.email;
+        let password = req.query.password;
+        const selected_user = get_user.by_mail(email);
+        
+        if (selected_user.user == null || selected_user.user.password !== password){
+            res.status(401).send({});
+        }
+        else{
+            req.session.account_id = selected_user.user.account_id
+            //res.json({}).end()
+            res.status(200).send({});
+            
+        }
+    });
+
+    return router
+}
